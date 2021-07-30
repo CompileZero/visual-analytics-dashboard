5f2795e4e98489cb478679b90280e69e3089e7d5
Atharva Kulkarni <atharvaak66@gmail.com>
2021-07-30 11:54:06 +0530

    removed a word

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -6,7 +6,7 @@ const Loading = () => {
 
             <div className="text-center">
                 <div className="spinner-border" role="status">
-                    <span className="visually-hidden">Loading New</span>
+                    <span className="visually-hidden">Loading</span>
                 </div>
             </div>
         </div>
