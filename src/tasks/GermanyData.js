71cd58590923d10ee2a916f8f49e1a91bbbf21fd
Atharva Kulkarni <atharvaak66@gmail.com>
2021-08-22 10:37:17 +0100

    added germany data historical

--- src/tasks/GermanyData.js
+++ src/tasks/GermanyData.js
@@ -3,20 +3,46 @@ class GermanyData {
 
     setState = null;
     covidData = [];
+    covidDataCases = [];
+    covidDataDeaths = [];
     mapRegions = features;
+
 
     getCovidData = async (url) => {
         const response = await fetch(url);
         let data = await response.json();
-
         this.covidData = data.data;
+        //console.log(Object.keys(this.covidData).length);
+    };
+
+    getCovidDataCases = async (url) => {
+        const response = await fetch(url);
+        let data = await response.json();
+
+        this.covidDataCases = data.data;
+        //console.log(Object.keys(this.covidData).length);
+    };
+
+    getCovidDataDeaths = async (url) => {
+        const response = await fetch(url);
+        let data = await response.json();
+
+        this.covidDataDeaths = data.data;
         //console.log(Object.keys(this.covidData).length);
     };
 
     loadDailyData = async (setRegions) => {
         await this.getCovidData("https://api.corona-zahlen.org/districts");
-        let arr = Object.values(this.covidData);
-        let max_cases = Math.max.apply(Math, arr.map(function (o) { return o.cases; }));
+        await this.getCovidDataCases("https://api.corona-zahlen.org/districts/history/cases/10");
+        await this.getCovidDataDeaths("https://api.corona-zahlen.org/districts/history/deaths/10");
+
+
+
+
+        let arrCovidData = Object.values(this.covidData);
+        let arrCovidDataCases = Object.values(this.covidDataCases);
+        let arrCovidDataDeaths = Object.values(this.covidDataDeaths);
+        let max_cases = Math.max.apply(Math, arrCovidData.map(function (o) { return o.cases; }));
         console.log(max_cases);
 
         // console.log(typeof (this.covidData));
@@ -26,7 +52,7 @@ class GermanyData {
         for (let i = 0; i < this.mapRegions.length; i++) {
             const mapDataRegion = this.mapRegions[i];
 
-            const covidDataRegion = arr.find((region) => {
+            const covidDataRegion = arrCovidData.find((region) => {
 
                 if (region.name.normalize("NFD").replace(/\p{Diacritic}/gu, "") == mapDataRegion.properties.NAME_3.normalize("NFD").replace(/\p{Diacritic}/gu, "")
                     || region.name.normalize("NFD").replace(/\p{Diacritic}/gu, "").includes(mapDataRegion.properties.NAME_3.normalize("NFD").replace(/\p{Diacritic}/gu, ""), 0)
@@ -40,6 +66,43 @@ class GermanyData {
             }
             );
             if (covidDataRegion) {
+
+
+                // console.log(arrCovidDataDeaths);
+
+                let covidDataRegionCases = arrCovidDataCases.find((region) => {
+                    if (region.name.normalize("NFD").replace(/\p{Diacritic}/gu, "") == mapDataRegion.properties.NAME_3.normalize("NFD").replace(/\p{Diacritic}/gu, "")
+                        || region.name.normalize("NFD").replace(/\p{Diacritic}/gu, "").includes(mapDataRegion.properties.NAME_3.normalize("NFD").replace(/\p{Diacritic}/gu, ""), 0)
+                        || mapDataRegion.properties.NAME_3.normalize("NFD").replace(/\p{Diacritic}/gu, "").includes(region.name.normalize("NFD").replace(/\p{Diacritic}/gu, ""), 0)
+                    ) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    };
+                });
+
+                let covidDataRegionDeaths = arrCovidDataDeaths.find((region) => {
+                    if (region.name.normalize("NFD").replace(/\p{Diacritic}/gu, "") == mapDataRegion.properties.NAME_3.normalize("NFD").replace(/\p{Diacritic}/gu, "")
+                        || region.name.normalize("NFD").replace(/\p{Diacritic}/gu, "").includes(mapDataRegion.properties.NAME_3.normalize("NFD").replace(/\p{Diacritic}/gu, ""), 0)
+                        || mapDataRegion.properties.NAME_3.normalize("NFD").replace(/\p{Diacritic}/gu, "").includes(region.name.normalize("NFD").replace(/\p{Diacritic}/gu, ""), 0)
+                    ) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    };
+                });
+
+                if (covidDataRegionCases) {
+                    this.mapRegions[i].properties.history_cases = covidDataRegionCases.history;
+                }
+
+                if (covidDataRegionDeaths) {
+                    this.mapRegions[i].properties.history_deaths = covidDataRegionDeaths.history;
+                }
+
+
                 countRegions += 1;
                 this.mapRegions[i].properties.cases = covidDataRegion.cases;
                 this.mapRegions[i].properties.population = covidDataRegion.population;
