2f575054c263ce73e660e12cb052ca9d4bb567fa
Atharva Kulkarni <atharvaak66@gmail.com>
2021-08-22 10:37:32 +0100

    historical data for germany component

--- src/components/CovidMap.js
+++ src/components/CovidMap.js
@@ -12,7 +12,7 @@ import "leaflet/dist/leaflet.css";
 import "./CovidMap.css";
 // import { divIcon, Point } from "leaflet";
 import FilterSelect from "./FilterSelect";
-import { colorCalculatorDeaths, colorCalculatorCases } from "../tasks/ColorCalculator.js";
+import { colorCalculatorDeaths, colorCalculatorCases, colorCalculatorSevenDayCases, colorCalculatorSevenDayDeaths } from "../tasks/ColorCalculator.js";
 
 const CovidMap = ({ regionsGermany, regionsNetherlands }) => {
 
@@ -23,10 +23,10 @@ const CovidMap = ({ regionsGermany, regionsNetherlands }) => {
     let seventhDay = today.toISOString().split("T")[0];
 
 
-    console.log(regionsNetherlands);
+
 
     let [metric, setMetric] = useState('Cases');
-    console.log(metric);
+
 
     let keyMap1 = Math.random();
     let keyMap2 = Math.random();
@@ -46,24 +46,43 @@ const CovidMap = ({ regionsGermany, regionsNetherlands }) => {
 
     const onEachRegion = (region, layer) => {
 
-        if (metric == "Total Cases") {
-            if (region.properties.cases) {
-                layer.options.fillColor = colorCalculatorCases(region.properties.cases);
-            }
-            else {
-                layer.options.color = "";
-                layer.options.fillOpacity = 0;
-            }
+        if (!region.properties.cases) {
+            layer.options.color = "";
+            layer.options.fillOpacity = 0;
+            return;
+        }
 
+        let sevenDayCases = 0;
+        if (region.properties.history_cases) {
+            region.properties.history_cases.forEach((item, index) => {
+                sevenDayCases += item.cases;
+            });
+            sevenDayCases = (sevenDayCases / region.properties.history_deaths.length).toFixed(2);
+        }
+
+        let sevenDayDeaths = 0;
+        if (region.properties.history_deaths) {
+            region.properties.history_deaths.forEach((item, index) => {
+
+                sevenDayDeaths += item.deaths;
+            });
+            sevenDayDeaths = (sevenDayDeaths / region.properties.history_deaths.length).toFixed(2);
+        }
+
+
+        if (metric == "Total Cases") {
+            layer.options.fillColor = colorCalculatorCases(region.properties.cases);
         }
         else if (metric == "Total Deaths") {
-            if (region.properties.deaths) {
-                layer.options.fillColor = colorCalculatorDeaths(region.properties.deaths);
-            }
-            else {
-                layer.options.color = "";
-                layer.options.fillOpacity = 0;
-            }
+            layer.options.fillColor = colorCalculatorDeaths(region.properties.deaths);
+        }
+
+        else if (metric == "Cases: 7 Day-Average") {
+            layer.options.fillColor = colorCalculatorSevenDayCases(sevenDayCases);
+        }
+        else if (metric == "Deaths: 7 Day-Average") {
+            layer.options.fillColor = colorCalculatorSevenDayDeaths(sevenDayDeaths);
+
         }
 
         const name = region.properties.NAME_3;
@@ -74,13 +93,18 @@ const CovidMap = ({ regionsGermany, regionsNetherlands }) => {
 
         layer.on('mouseover', function (e) {
 
-            layer.bindPopup(`<h6 style='font-size:12px;'>
-            <strong>${name}</strong>
-            </h6>
-            <div>Population<h6 style='font-size:12px;'>${population}</h6></div>
-            <div>Confirmed Cases<h6 style='font-size:12px;'>${confirmedText}</h6></div>
-            <div>Deaths<h6 style='font-size:12px;'>${deaths}</h6></div>
-            <div>Recovered<h6 style='font-size:12px;'>${recovered}</h6></div>`, { minWidth: 100 }).openPopup(e.latlng);
+            layer.bindPopup(`
+            <h6 class='text-base font-bold text-blue-800'>
+            <strong>${name}</strong></h6>
+            <div class='text-md font-bold text-gray-400'>Confirmed Cases</div><h6 class='text-base font-bold text-yellow-800'>${confirmedText}</h6>
+            <div class='text-md font-bold text-gray-400'>Population</div><h6 class='text-base font-bold text-yellow-600'>${population}</h6>
+            <div class='text-md font-bold text-gray-400'>Deaths</div><h6 class='text-base font-bold text-pink-500'>${deaths}</h6>
+            <div class='text-md font-bold text-gray-400'>Recovered</div><h6 class='text-base font-bold text-pink-500'>${recovered}</h6>
+            <hr/>
+            <div class='text-lg font-medium text-gray-400'>7 Day Average</div>
+            <div class='text-md font-bold text-gray-400'>Cases <span class='text-base font-bold text-gray-700'>${sevenDayCases}</span></div>
+            <div class='text-md font-bold text-gray-400'>Deaths <span class='text-base font-bold text-gray-700'>${sevenDayDeaths}</span></div>`
+                , { minWidth: 100 }).openPopup(e.latlng);
         });
 
     };
@@ -96,8 +120,8 @@ const CovidMap = ({ regionsGermany, regionsNetherlands }) => {
         let historyData = region.properties["History_data"];
 
 
-        let sevenDayCases = (historyData[6]["Total_reported"] - historyData[0]["Total_reported"]) / 7;
-        let sevendDayDeaths = (historyData[6]["Deceased"] - historyData[0]["Deceased"]) / 7;
+        let sevenDayCases = ((historyData[6]["Total_reported"] - historyData[0]["Total_reported"]) / 7).toFixed(2);
+        let sevenDayDeaths = ((historyData[6]["Deceased"] - historyData[0]["Deceased"]) / 7).toFixed(2);
 
         if (metric == "Total Cases") {
 
@@ -112,17 +136,33 @@ const CovidMap = ({ regionsGermany, regionsNetherlands }) => {
 
         }
 
+        else if (metric == "Cases: 7 Day-Average") {
+
+            layer.options.fillColor = colorCalculatorSevenDayCases(sevenDayCases);
+        }
+        else if (metric == "Deaths: 7 Day-Average") {
+            layer.options.fillColor = colorCalculatorSevenDayDeaths(sevenDayDeaths);
+        }
+
         const name = region.properties.NAME_2;
         // const cases = historyData[6]["Total_reported"]
         const confirmedText = historyData[6]["Total_reported"];
         const hospital_admission = historyData[6]["Hospital_admission"];
         const deceased = historyData[6]["Deceased"];
         layer.on('mouseover', function (e) {
-            layer.bindPopup(`<h6 class='text-lg font-bold text-blue-800'>
+            layer.bindPopup(`
+            <h6 class='text-base font-bold text-blue-800'>
             <strong>${name}</strong></h6>
-            <div class='text-md font-bold text-gray-700'>Confirmed Cases</div><h6 class='text-lg font-bold text-yellow-800'>${confirmedText}</h6>
-            <div class='text-md font-bold text-gray-700'>Hospital Admissions</div><h6 class='text-lg font-bold text-yellow-600'>${hospital_admission}</h6>
-            <div class='text-md font-bold text-gray-700'>Deaths</div><h6 class='text-lg font-bold text-pink-500'>${deceased}</h6>`, { minWidth: 80 }).openPopup(e.latlng);
+            <div class='text-md font-bold text-gray-400'>Confirmed Cases</div><h6 class='text-base font-bold text-yellow-800'>${confirmedText}</h6>
+            <div class='text-md font-bold text-gray-400'>Hospital Admissions</div><h6 class='text-base font-bold text-yellow-600'>${hospital_admission}</h6>
+            <div class='text-md font-bold text-gray-400'>Deaths</div><h6 class='text-base font-bold text-pink-500'>${deceased}</h6>
+            <hr/>
+            <div class='text-lg font-medium text-gray-400'>7 Day Average</div>
+            <div class='text-md font-bold text-gray-400'>Cases <span class='text-base font-bold text-gray-700'>${sevenDayCases}</span></div>
+            <div class='text-md font-bold text-gray-400'>Deaths <span class='text-base font-bold text-gray-700'>${sevenDayDeaths}</span></div>
+            `,
+
+                { minWidth: 80 }).openPopup(e.latlng);
         });
 
     };
@@ -150,7 +190,7 @@ const CovidMap = ({ regionsGermany, regionsNetherlands }) => {
                         {/* {<GeoJSON style={mapStyle} data={belgium_regions} onEachFeature={onEachRegion} />} */}
                         {/* {<GeoJSON style={mapStyleBorder} data={germany_border} />}
                         {<GeoJSON style={mapStyleBorder} data={netherlands_border} />} */}
-                        {console.log(metric)}
+
                         <GeoJSON key={keyMap1} style={mapStyle} data={regionsGermany} onEachFeature={onEachRegion} />
                         <GeoJSON key={keyMap2} style={mapStyle} data={regionsNetherlands} onEachFeature={onEachRegionNetherlands} />
                         {/* {metric == "Cases" ? <GeoJSON key={1} style={mapStyle} data={regionsGermany} onEachFeature={onEachRegion} /> : <GeoJSON key={2} style={mapStyle} data={regionsNetherlands} onEachFeature={onEachRegionNetherlands} />} */}
