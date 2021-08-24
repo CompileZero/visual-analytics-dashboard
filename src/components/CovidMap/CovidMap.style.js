75300373650ad8a1598550bd4388c76ae61798a2
Atharva Kulkarni <atharvaak66@gmail.com>
2021-08-24 07:29:45 +0100

    refactoring to multiple source files

---
+++ src/components/CovidMap/CovidMap.style.js
@@ -0,0 +1,11 @@
+export const mapStyle = {
+    weight: 0.8,
+    color: "#FFFFFF",
+    fillOpacity: 0.9
+};
+
+export const mapStyleBorder = {
+    color: "#0b0038",
+    weight: 1,
+    fillOpacity: 0
+};
