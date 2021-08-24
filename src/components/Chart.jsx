536baaed4e88960a37f8d8d73f003faefdb561b8
Atharva Kulkarni <atharvaak66@gmail.com>
2021-08-24 17:12:11 +0100

    added germany and netherlands charts

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,29 +1,97 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import { dateForecast } from "../tasks/dateForecast.js";
+import { smaForecast } from '../tasks/smaForecast.js';
 
-const Chart = ({ chartData = [] }) => {
+var sma = require('sma');
 
-    console.log(chartData);
-    const data = {
+const Chart = ({ chartData = {} }) => {
+    let data = {};
+    // console.log(chartData);
+    if (chartData.country == "Germany") {
+        let displayData = chartData.data.map(row => row.cases);
+        let labels = chartData.data.map(row => { let date = row.date.split("T")[0]; return date; });
 
-        labels: chartData.map(row => row.date),
-        datasets: [
-            {
-                label: '# of Votes',
-                data: chartData.map(row => row.cases),
-                fill: false,
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgba(255, 99, 132, 0.2)',
-            },
-        ],
-    };
+        let dateLabels = [];
+        if (labels.length != 0) dateLabels = dateForecast(labels, 40);
+        let smaData = smaForecast(displayData);
+
+        data = {
+
+            labels: dateLabels,
+            datasets: [
+                {
+                    label: '# of Reported Cases',
+                    data: displayData,
+                    fill: false,
+                    backgroundColor: 'rgb(255, 99, 132,0.2)',
+                    borderColor: 'rgba(255, 99, 132, 0.2)',
+                    fill: true
+                },
+
+                {
+                    label: 'SMA Forecast Trend',
+                    data: smaData,
+                    fill: false,
+                    backgroundColor: 'rgb(245, 226, 59, 0.8)',
+                    borderColor: 'rgba(245, 177, 59, 0.9)',
+                    fill: false
+                }
+            ],
+        };
+    }
+    else if (chartData.country == "Netherlands") {
+        console.log(chartData.data);
+        let bufferCases = chartData.data[0]["Total_reported"];
+        let displayData = chartData.data.map(row => {
+            let newCases = row["Total_reported"] - bufferCases;
+
+            bufferCases = row["Total_reported"];
+            return newCases;
+        });
+
+        let labels = chartData.data.map(row => { let date = row["Date_of_report"].split(" ")[0]; return date; });
+
+        let dateLabels = [];
+        if (labels.length != 0) dateLabels = dateForecast(labels, 40);
+        let smaData = smaForecast(displayData);
+
+        data = {
+
+            labels: dateLabels,
+            datasets: [
+                {
+                    label: '# of Reported Cases',
+                    data: displayData,
+                    fill: false,
+                    backgroundColor: 'rgb(255, 99, 132,0.2)',
+                    borderColor: 'rgba(255, 99, 132, 0.2)',
+                    fill: true
+                },
+
+                {
+                    label: 'SMA Forecast Trend',
+                    data: smaData,
+                    fill: false,
+                    backgroundColor: 'rgb(245, 226, 59, 0.8)',
+                    borderColor: 'rgba(245, 177, 59, 0.9)',
+                    fill: false
+                }
+            ],
+        };
+    }
+
 
     const options = {
+        fillColor: 'rgba(255, 99, 132, 0.2)',
+        tension: 0.2,
         scales: {
+
             yAxes: [
                 {
                     ticks: {
                         beginAtZero: true,
+                        padding: 0
                     },
                 },
             ],
@@ -32,7 +100,8 @@ const Chart = ({ chartData = [] }) => {
 
     return (
         <div>
-            <Line data={data} options={options} />
+            <Line height={50} data={data} options={options} />
+
         </div>
     );
 };
